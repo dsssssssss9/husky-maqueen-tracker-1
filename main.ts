55b7e42ca723a951357eff633532440ab6f0c814
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2023-01-25 23:41:31 +0000

    Untested Test code to display x cord on Husky Screen

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+huskylens.initI2c()
+huskylens.initMode(protocolAlgorithm.ALGORITHM_OBJECT_TRACKING)
 basic.forever(function () {
-	
+    huskylens.request()
+    huskylens.writeOSD(convertToText(huskylens.readeBox(1, Content1.xCenter)), 150, 30)
 })
